a9279eff363b522dddfd08c941456e89e1affc8e
hercs <xhercs@gmail.com>
2015-12-19 04:55:38 +0100

    Animaciones del menu

--- main.js
+++ main.js
@@ -1,31 +1,40 @@
 // main.js
 
 var Nav = React.createClass({
-				getInitialState: function () {
-					return { home: "Home",
-								me: "About Me",
-								work: "My Work",
-								contact: "Contact Me"
+						
+			getInitialState: function () {
+					return { home: "home",
+							   view: "hide"
 					};
 				},
+						  
 				goHome: function () {
-					this.setState({
-								home: "En un lugar de la mancha",
-								me: "Esto es solo sobre mi",
-					})
+					if (this.state.home==="home")
+					{ 
+						this.setState({ home: "homeonClick",
+											 view: "show"
+										})
+						
+					}	else {
+						this.setState({ home: "home",
+											 view: "hide"									
+										})
+
+					}
 				},
+
 				render: function(){
 					return (
 							<ul>
-								<li onClick={this.goHome}>{this.state.home}</li>
-								<li>{this.state.me}</li>
-								<li>{this.state.work}</li>
-								<li>{this.state.contact}</li>
-								<li>atras</li>
-								<li>adelante</li>
-							</ul>	
-					);				
+								<li className="main" id={this.state.home} onClick={this.goHome}><a href="#">HOME</a></li>
+	          					<li className={this.state.view}><a href="#">About Me</a></li>
+									<li className={this.state.view}><a href="#">My Work</a></li>
+									<li className={this.state.view}><a href="#">Contact Me</a></li>
+									<li>atras</li>
+									<li>adelante</li>
+							</ul>
+					);		
 				}		
 		});
 		
-ReactDOM.render(<Nav/>,document.getElementById("mainav"));
+ReactDOM.render(<Nav/>,document.getElementById("menu"));
