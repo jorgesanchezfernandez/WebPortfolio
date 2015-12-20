5912375bc1e0c49c460b4a84f6e2904dd7712c3a
hercs <xhercs@gmail.com>
2015-12-20 18:49:51 +0100

    Menu interactivo completado

--- main.js
+++ main.js
@@ -1,49 +1,98 @@
 // main.js
 
 var Nav = React.createClass({
-						
-			getInitialState: function () {
-					return { home: "home",
-							   view: "hide"
-					};
+			
+			  //Initialize states
+			  getInitialState: function () {
+					return {  
+					          menuAct: false ,          //Menu activate or inactivate
+					         // sectionAct: false, 
+								 btnMeAct: false,          //Button activate or inactivate
+								 btnWorkAct: false,        //Button activate or inactivate
+								 btnContactAct: false		//Button activate or inactivate			 
+							 };
+													
 				},
 						  
+				//If Menu was clicked
 				goHome: function () {
-					if (this.state.home==="home")
-					{ 
-						this.setState({ home: "homeonClick",
-											 view: "show"
-										})
-						
-					}	else {
-						this.setState({ home: "home",
-											 view: "hide"									
-										})
+					
+					this.setState({ menuAct: !this.state.menuAct, 
+										 btnMeAct: false,        //Inactive button
+										 btnWorkAct: false,      //Inactive button
+										 btnContactAct: false    //Inactive button
+									  })			
+				},
 
+				//If About me was clicked
+				goMe: function () {
+					
+					if (!this.state.btnMeAct)                 //Do if it's not already actived
+					{
+						this.setState({ btnMeAct: true,        //Active
+											 btnWorkAct: false,     //Inactive
+											 btnContactAct: false	//inactive
+										  })
 					}
+						 
+				},
+				
+				//If My Work was clicked
+				goWork: function () {
+					if (!this.state.btnWorkAct)					//Do if it's not already actived
+					{	
+						this.setState({ btnWorkAct: true,      //Active
+						    				 btnMeAct: false,       //Inactive
+											 btnContactAct: false	//inactive
+						              })
+					}
+				},
+				
+				//If Contact Me was clicked
+				goContact: function () {
+					
+					if (!this.state.btnContactAct)                  //Do if it's not already actived
+					{
+						this.setState({ btnContactAct: true,    //Active
+											 btnMeAct: false,        //Active
+											 btnWorkAct: false,      //Inactive
+										  })
+					}
+						 
 				},
 
 				render: function(){
+					//Building data-names
+					var menuActName = "menuAct" + this.state.menuAct;
+					
 					return (
 							<ul>
-								<li className="main" id={this.state.home} onClick={this.goHome}><a href="#"><img src="images/menu.png" /></a></li>
-	          					<li className={this.state.view}><a href="#">About Me</a></li>
-									<li className={this.state.view}><a href="#">My Work</a></li>
-									<li className={this.state.view}><a href="#">Contact Me</a></li>
-									<li>atras</li>
-									<li>adelante</li>
+								<li onClick={this.goHome}><img src="images/menu.png" /></li>
+          					<li data-menu={menuActName} data-active={this.state.btnMeAct} onClick={this.goMe}>About Me</li>
+								<li data-menu={menuActName} data-active={this.state.btnWorkAct} onClick={this.goWork}>My Work</li>
+								<li data-menu={menuActName} data-active={this.state.btnContactAct} onClick={this.goContact}>Contact Me</li>
 							</ul>
 					);		
 				}		
 		});
+		
+var Me = React.createClass({
+						
+	render: function(){
+		return (
+				<p>Hola esto es el pie</p>
+		);		
+	}		
+});
 
 var Foot = React.createClass({
 						
-				render: function(){
-					return (
-							<p>Hola esto es el pie</p>
-					);		
-				}		
-		});
+	render: function(){
+		return (
+				<p>Hola esto es el pie</p>
+		);		
+	}		
+});
 ReactDOM.render(<Nav/>,document.getElementById("menu"));
+ReactDOM.render(<Me/>,document.getElementById("me"));
 ReactDOM.render(<Foot/>,document.getElementById("footmain"));
